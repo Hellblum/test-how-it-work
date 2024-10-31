741076e8733b8253d2e7b08892000c4d37cf2b81
Mykhailo <myscreamo@gmail.com>
2024-10-31 20:40:24 +0100

    created metod at js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const users = [
+	{
+		name: "Mykhailo",
+		age: 32,
+		sex: "male",
+	},
+	{
+		name: "Marharyta",
+		age: 24,
+		sex: "female",
+	},
+	{
+		name: "Ilia",
+		age: 26,
+		sex: "male",
+	},
+	{
+		name: "Sara",
+		age: 17,
+		sex: "female",
+	},
+	{
+		name: "Petro",
+		age: 14,
+		sex: "male",
+	},
+];
+
+// const correctNames = users.map(function (item) {
+// 	return item.sex === "male" ? "Mr. " + item.name : "Mrs." + item.name;
+// });
+
+const sum = users.reduce(function (accumulator, currentValue) {
+	return accumulator + currentValue.age;
+}, 0);
+// console.log(correctNames);
+// console.log(users);
+
+// console.log(sum/users.length);
+
+const findObj = users.find(function (item) {
+	return item.name === "Mykhailo";
+});
+
+console.log(findObj);
+
 
